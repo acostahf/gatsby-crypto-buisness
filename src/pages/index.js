2f6d6920d575d3491299dbf7fb65473591d16b3b
Fabian A <fabian@Fabians-MacBook-Pro.local>
2021-08-31 14:36:58 -0500

    styling buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,18 @@ const IndexPage = () => (
   <Layout className="p-0">
     <Seo title="Home" />
     {/* div that has the hero */}
-    <div className='max-w-full p-0 bg-hero-pic h-96 bg-cover '>
+    <div className='max-w-full p-0 bg-hero-pic h-96 bg-cover bg-blend-overlay '>
       {/* heading */}
-    <div className="flex justify-center items-center h-96 ">
-      <h1 className="text-white text-3xl">Welcome to Crypto Buisness</h1>
+    <div className="flex justify-center flex-col  items-center h-96 ">
+      <h1 className="text-white text-3xl">An avenue for the evolution of Defi</h1>
+      <h1 className='text-blue-200'>A new way of minin crypto. Invest by holding.</h1>
+      <div className=" mt-10">
+        
+      <button className="bg-gradient-to-r from-green-400 to-blue-500 rounded text-white text-md px-2 py-1 mr-4 flex  ">Lauch app <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-4 p-0 my-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+</svg></button>
+      <button className=" bg-white backdrop-filter backdrop-blur-xl bg-opacity-30 rounded text-purple-800 text-md px-2 py-1  ">Read Docs</button>
+      </div>
     </div>
     </div>
 {/* section with 3 cards */}
