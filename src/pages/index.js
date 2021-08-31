2c38e4a3b04f60d8d8795a78a6939683c0485089
Fabian A <fabian@Fabians-MacBook-Pro.local>
2021-08-31 14:46:34 -0500

    add svgs to buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,17 @@ const IndexPage = () => (
     {/* div that has the hero */}
     <div className='max-w-full p-0 bg-hero-pic h-96 bg-cover bg-blend-overlay '>
       {/* heading */}
-    <div className="flex justify-center flex-col  items-center h-96 ">
+    <div className="flex justify-center flex-col items-center h-96 w-full ">
       <h1 className="text-white text-3xl">An avenue for the evolution of Defi</h1>
       <h1 className='text-blue-200'>A new way of minin crypto. Invest by holding.</h1>
-      <div className=" mt-10">
+      <div className=" mt-10 w-full justify-center flex">
         
       <button className="bg-gradient-to-r from-green-400 to-blue-500 rounded text-white text-md px-2 py-1 mr-4 flex  ">Lauch app <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-4 p-0 my-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
 </svg></button>
-      <button className=" bg-white backdrop-filter backdrop-blur-xl bg-opacity-30 rounded text-purple-800 text-md px-2 py-1  ">Read Docs</button>
+      <button className=" bg-white backdrop-filter backdrop-blur-xl bg-opacity-30 rounded text-blue-300 text-md px-2 py-1 flex   ">Read Docs<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+</svg></button>
       </div>
     </div>
     </div>
