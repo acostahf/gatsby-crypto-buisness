e77a79ea7305c24de8168bd5635d32eb3d8811cc
Fabian <acostahf4@gmail.com>
2021-09-21 14:02:39 -0500

    fix responsiveness

--- src/components/card-section.js
+++ src/components/card-section.js
@@ -4,16 +4,18 @@ import Card from "./card"
 function Heading() {
   return (
     <div>
-      <h1 className="pt-10 text-4xl font-bold text-white ">FEATURES</h1>
+      <h1 className="pt-10 text-4xl font-bold text-white text-center ">
+        FEATURES
+      </h1>
     </div>
   )
 }
 // this function acts as a wropper around the cards in order to style the layout
 export default function cardSection(props) {
   return (
-    <div className=" w-full  p-10 flex flex-col justify-center items-center ">
+    <div className=" w-full p-20 flex flex-col justify-center items-center space-x-20">
       <Heading />
-      <div className=" flex flex-wrap p-10 justify-center items-center flex-auto">
+      <div className=" flex flex-wrap p-10 justify-center items-center flex-auto ">
         <Card {...props} />
         <Card {...props} />
         <Card {...props} />
