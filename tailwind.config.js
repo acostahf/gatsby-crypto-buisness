da37ed11277c7739d34fe4865560c61b9c6e6f85
Fabian <acostahf4@gmail.com>
2021-09-21 13:47:34 -0500

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,43 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     backdropFilter: {
-      'none': 'none',
-      'blur': 'blur(20px)',
+      none: "none",
+      blur: "blur(32px)",
+    },
+    height: {
+      300: "300px",
+      600: "600px",
     },
     scale: {
-      '0': '0',
-     '25': '.25',
-      '50': '.5',
-      '75': '.75',
-      '90': '.9',
-     '95': '.95',
-      '100': '1',
-     '105': '1.05',
-     '110': '1.1',
-      '125': '1.25',
-      '150': '1.5',
-     '200': '2',
+      0: "0",
+      25: ".25",
+      50: ".5",
+      75: ".75",
+      90: ".9",
+      95: ".95",
+      100: "1",
+      105: "1.05",
+      110: "1.1",
+      125: "1.25",
+      150: "1.5",
+      200: "2",
     },
-    //overwrites the theme font to roboto 
+    //overwrites the theme font to roboto
     //to add a new font use npm install typeface-roboto
     extend: {
       fontFamily: {
-        sans: ["roboto",
-         ...defaultTheme.fontFamily.sans,
-        ]
+        sans: ["roboto", ...defaultTheme.fontFamily.sans],
       },
-      transform: ['hover', 'focus'],
-      animation: ['motion-reduce'],
-      scale: ['active', 'group-hover'],
+      transform: ["hover", "focus"],
+      animation: ["motion-reduce"],
+      scale: ["active", "group-hover"],
       backgroundImage: theme => ({
-        'hero-pic': "url('../images/hero-pic.jpg')",
-        
-       }),
+        "hero-pic": "url('../images/hero-pic.jpg')",
+      }),
     },
     // fontFamily: {
     //   sans: ['Graphik', 'sans-serif'],
@@ -49,7 +47,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss-filters'),
-  ],
+  plugins: [require("tailwindcss-filters")],
 }
