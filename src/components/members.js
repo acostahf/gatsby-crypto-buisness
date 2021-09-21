da7796eb5a5fe2057bbc94464e2b5dacf048c8ce
Fabian <acostahf4@gmail.com>
2021-09-21 13:50:16 -0500

    created this component to show the count of followers on discord and twitter

---
+++ src/components/members.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Members = () => {
+  return (
+    <div className="flex justify-center space-10 bg-white w-full bg-opacity- bg-opacity-25 backdrop-filter-blur h-60 ">
+      <div className="w-1/2 flex items-center justify-center flex-col ">
+        <h1 className="text-5xl font-bold">4,236+</h1>
+        <h1>Discord Members</h1>
+      </div>
+      <div className="w-1/2 flex items-center justify-center flex-col ">
+        <h1 className="text-5xl font-bold ">2,343+</h1>
+        <h1>Twitter Members</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Members
