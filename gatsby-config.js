692affeb19470f5e6729b8cfe4f42a38a23b33fd
Fabian <acostahf4@gmail.com>
2021-09-22 12:58:20 -0500

    name change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Crypto Speedstars`,
+    title: `Crypto NFT`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Fabian`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
